313c9475e7c0db80f2c9283dfbc63b18cadee19c
shlokk9763 <30517216+shlokk9763@users.noreply.github.com>
2018-06-07 21:28:29 +0530

    Create First_Class.java

---
+++ Java/Second_day/First_Class.java
@@ -0,0 +1,12 @@
+
+public class First_Class {
+
+	public static void main(String[] args) {
+	
+		String one= "hii";
+		int two= 20;
+		int three=30;
+		System.out.println(one + " " + two + three);
+	}
+	
+}
